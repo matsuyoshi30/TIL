b30e5cbd145f4be4a0a93466a50f43bab9069802
matsuyoshi <sfbgwm30@gmail.com>
2019-12-07 23:26:23 +0900

    Update: React component.

--- react/main.jsx
+++ react/main.jsx
@@ -1,7 +1,6 @@
 function App() {
   return <div><h1>Hello React!</h1></div>;
 }
-
 const target = document.querySelector('#app');
 ReactDOM.render(<App/>, target);
 
@@ -14,3 +13,29 @@ function tick() {
   ReactDOM.render(element, document.getElementById('root'));
 }
 setInterval(tick, 1000);
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('propsample')
+);
+
+function Welcome2(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+function App2() {
+  return (
+    <div>
+      <Welcome2 name="Sara" />
+      <Welcome2 name="Cahal" />
+      <Welcome2 name="Edite" />
+    </div>
+  );
+}
+ReactDOM.render(
+  <App2 />,
+  document.getElementById('propsample2')
+);
