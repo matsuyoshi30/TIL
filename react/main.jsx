878ddc3e321efe9852204a085d1bd1497c42383b
matsuyoshi <sfbgwm30@gmail.com>
2019-12-08 23:27:46 +0900

    Update: React state and lifecycle.

--- react/main.jsx
+++ react/main.jsx
@@ -39,3 +39,42 @@ ReactDOM.render(
   <App2 />,
   document.getElementById('propsample2')
 );
+
+// -------------------
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('clock')
+);
