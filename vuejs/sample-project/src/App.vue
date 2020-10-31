9a91671450a029583f2ec164d40bb1f758517c69
matsuyoshi <sfbgwm30@gmail.com>
2020-10-31 23:31:35 +0900

    Add vue sample project

---
+++ vuejs/sample-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <Counter name="Counter 1" :initCount="5" />
+        <Counter name="Counter 2" :initCount="10" />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+      HelloWorld,
+      Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuejs/sample-project/src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="counter">
+        <p>{{ name }}</p>
+        <p>Count: {{ count }}</p>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        name: String,
+        initCount: Number
+    },
+    data() {
+        return {
+            count: this.initCount
+        }
+    },
+    method: {
+        increment() {
+            this.count++;
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.counter {
+    width: 200px;
+}
+</style>
