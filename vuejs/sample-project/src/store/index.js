f3a4ca708cb5141770839a986593dd2bf6ce1716
matsuyoshi <sfbgwm30@gmail.com>
2020-11-01 23:44:09 +0900

    Vue: vuex で api 通信管理

--- vuejs/sample-project/src/store/index.js
+++ vuejs/sample-project/src/store/index.js
@@ -5,14 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    globalCount: 0
+    profile: null,
+    globalCount: 0,
   },
   mutations: {
     globalIncrement(state) {
       state.globalCount++
-    }
+    },
+    setGitHubProfile(state, payload) {
+      state.profile = payload;
+    },
   },
   actions: {
+    async fetchGitHubProfile(store, payload) {
+      if (store.state.profile !== null) return;
+
+      const response = await fetch(
+        `https://api.github.com/users/${payload.user_id}`
+      ).then((res) => {
+        return res.json();
+      });
+      store.commit("setGitHubProfile", response);
+    },
   },
   modules: {
   }
