6c476bce04c19cd2cdea16ef63e1f9f420b6f226
matsuyoshi <sfbgwm30@gmail.com>
2020-11-01 11:22:35 +0900

    Vue: コンポ間のデータ連携（ライブラリなし）

--- vuejs/sample-project/src/views/Count.vue
+++ vuejs/sample-project/src/views/Count.vue
@@ -2,12 +2,15 @@
     <div>
         <Counter name="Counter 1" :initCount="5" @emitUp="getEvent" />
         <Counter name="Counter 2" :initCount="10" @emitUp="getEvent" />
+        <p>{{ primitiveStore.$data.globalCount }}</p>
+        <input type="text" v-model="primitiveStore.$data.globalCount">
         {{ stack }}
     </div>
 </template>
 
 <script>
 import Counter from '@/components/Counter.vue'
+import primitiveStore from '@/primitiveStore.js'
 
 export default {
     components: {
@@ -15,12 +18,14 @@ export default {
     },
     data() {
         return {
-            stack: []
+            stack: [],
+            primitiveStore
         }
     },
     methods: {
         getEvent(payload) {
-            this.stack.push(payload)
+            this.stack.push(payload);
+            this.primitiveStore.$data.globalCount++;
         }
     }
 }
